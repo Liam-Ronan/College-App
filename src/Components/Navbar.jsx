ab00af2c3d919532b98c25bd0ded8ab1e70bf32e
Liam Ronan <liamronan16@gmail.com>
2023-12-01 13:52:19 +0000

    Navbar finished

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,11 @@
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../Contexts/AuthContext';
 
+//Images
 import gradPNG from '../../public/graduation-hat.png';
+
+//Icons
+import { BiLogOut } from "react-icons/bi";
 
 const Navbar = () => {
     const { authenticated, onAuthenticated } = useAuth();
@@ -17,24 +21,28 @@ const Navbar = () => {
         <>
             {(authenticated) ? (
                 <>
-                <div className='flex justify-evenly'>
-                    <div>
-                        <img src={gradPNG} alt="" className="h-32 w-32 p-2" />
-                        <h1 className='text-4xl text-white font-black p-2'>Enrolment <strong className='font-colour underline underline-offset-8 font-bold'>EcoSystem</strong></h1>
+                    <div className='flex justify-evenly items-center bg-gray-900 text-white pt-6 pb-6'>
+                        <div>
+                            <Link to="/Home" className='flex items-center'>
+                                <img src={gradPNG} alt="" className="h-16 w-16 p-3" />
+                                <h1 className='text-2xl text-white font-light pb-2'>Enrolment <strong className='font-colour font-bold slider-link pb-1'>EcoSystem</strong></h1>
+                            </Link>
+                        </div>
+                    
+                        <div className='font-bold'>
+                            <Link to="/Courses" className='slider-link mr-5 pb-1'>Courses</Link>
+                            <Link to="/Lecturers" className='slider-link mr-5 pb-1'>Lecturers</Link>
+                            <Link to="/Enrolments" className='slider-link  mr-5 pb-1'>Enrolments</Link>
+                        </div>
+
+                        <div className='font-bold'>
+                            <button className='flex slider-link pb-1' onClick={logout}><BiLogOut className='text-2xl mr-1'/> Logout</button>
+                        </div>
                     </div>
-                </div>
-                    <Link to="/Home">Home</Link> |
-                    <Link to="/Courses">All Courses</Link> |
-                    <Link to="/Lecturers">All Lecturers</Link> |
-                    <Link to="/Enrolments">All Enrolments</Link> |
+
                 </>
 
             ) : null}
-
-
-            {(authenticated) ? (
-                <button onClick={logout}>Logout</button>
-            ) : ""}
 
         </>
     );
--- src/Pages/LoginForm.jsx
+++ src/Pages/LoginForm.jsx
@@ -2,10 +2,15 @@ import axios from 'axios';
 import { useState } from 'react';
 import { useAuth } from '../Contexts/AuthContext';
 import { useNavigate } from 'react-router-dom';
+
+//CSS
 import '../App.css'
 
+//Images
 import CollegeSVG from '../Assets/Online learning-cuate.svg';
 import gradPNG from '../../public/graduation-hat.png';
+
+
 
 const LoginForm = () => {
     const { onAuthenticated } = useAuth();
@@ -52,14 +57,14 @@ const LoginForm = () => {
     return (
         <div className="flex h-screen items-center">
 
-            <div className="flex-1 w-1/2 h-full bg-gray-800 flex items-center justify-center flex-col">
+            <div className="flex-1 w-1/2 h-full bg-gray-900 flex items-center justify-center flex-col">
                 <img src={CollegeSVG} alt="" className="h-3/5 w-4/5" />
                 <h1 className='text-4xl text-white font-light p-2'>Enrolment <strong className='font-colour underline underline-offset-8 font-bold'>EcoSystem</strong></h1>
                 <p className='text-center max-w-xl text-xl p-2 text-white font-sans'>Navigate a rich landscape of courses and resources, where each decision opens doors to new learning and growth.</p>
             </div>
 
             <div className="flex-1 flex items-center justify-center">
-                <div className="max-w-xl p-8 bg-white ring ring-gray-300 ring-opacity-50 shadow-xl rounded-xl p-16 w-full">
+                <div className="max-w-xl p-8 bg-white ring ring-gray-300 ring-opacity-50 shadow-xl rounded-xl p-8 w-full">
                     {/* Your login form HTML goes here */}
                     <img src={gradPNG} alt="" className="mx-auto h-32 w-32 p-2" />
                     <h2 className="text-center text-5xl font-light mb-4 font-sans p-2">Welcome <strong className='font-colour font-bold underline underline-offset-8'>Back</strong></h2>
