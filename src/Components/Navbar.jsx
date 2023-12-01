c9e4b5e035deb0c0f53afbe1a149c442cadcb39c
Liam Ronan <liamronan16@gmail.com>
2023-12-01 12:06:57 +0000

    styling navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,8 @@
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../Contexts/AuthContext';
 
+import gradPNG from '../../public/graduation-hat.png';
+
 const Navbar = () => {
     const { authenticated, onAuthenticated } = useAuth();
 
@@ -15,6 +17,12 @@ const Navbar = () => {
         <>
             {(authenticated) ? (
                 <>
+                <div className='flex justify-evenly'>
+                    <div>
+                        <img src={gradPNG} alt="" className="h-32 w-32 p-2" />
+                        <h1 className='text-4xl text-white font-black p-2'>Enrolment <strong className='font-colour underline underline-offset-8 font-bold'>EcoSystem</strong></h1>
+                    </div>
+                </div>
                     <Link to="/Home">Home</Link> |
                     <Link to="/Courses">All Courses</Link> |
                     <Link to="/Lecturers">All Lecturers</Link> |
